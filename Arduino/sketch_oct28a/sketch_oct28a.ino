0cacd2786da0bcf25aeb5141973a8c39655f9afa
integralophie <sophiewu118@hotmail.com>
2019-10-29 13:47:32 -0400

    adding pseudo code for controlling motor

--- Arduino/sketch_oct28a/sketch_oct28a.ino
+++ Arduino/sketch_oct28a/sketch_oct28a.ino
@@ -1,9 +1,22 @@
 const int mosfet = 6;
+const int tran = 5;
 
 void setup() {
 pinMode(mosfet, OUTPUT);
+pinMode(tran, OUTPUT);
 }
 
 void loop() {
-analogWrite(6, 153);
+  if(btSignal == True) {
+    analogWrite(6, 153);
+    analogWrite(5, 153);
+    }
+
+  else {
+    analogWrite(6,0);
+    analogWrite(5,0);
+    }
+    
+  
+
 }
